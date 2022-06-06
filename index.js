96c91adcdddcda11172d6b0a6a46d9a81e9e9360
Imole97 <imoleadebanjo97@gmail.com>
2022-06-06 23:18:08 +0100

    removed replaceTemplate function from index.js

--- index.js
+++ index.js
@@ -33,23 +33,6 @@ const replaceTemplate = require('./modules/replaceTemplate')
 
 ////////////////////////////////////////////////
 //SERVER
-const replaceTemplate = (temp, product) => {
-    let output =  temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-    output  =  output.replace(/{%IMAGE%}/g, product.image)
-    output  =  output.replace(/{%PRICE%}/g, product.price)
-    output  =  output.replace(/{%FROM%}/g, product.from)
-    output  =  output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-    output  =  output.replace(/{%QUANTITY%}/g, product.quantity)
-    output  =  output.replace(/{%DESCRIPTION%}/g, product.description)
-    output  =  output.replace(/{%ID%}/g, product.id)
-
-    if(!product.organic) {output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')}
-
-    console.log(output)
-    return output
-    
-}
-
 const tempOverview = fs.readFileSync(`${__dirname}/template/template-overview.html`, 'utf-8')
 const tempCard = fs.readFileSync(`${__dirname}/template/template-card.html`, 'utf-8')
 // console.log(tempCard)
